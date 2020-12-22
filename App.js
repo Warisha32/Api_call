51935bb4bd725b1eda65a1a3dc89f9af78e77458
Warisha32 <66689362+Warisha32@users.noreply.github.com>
2020-12-22 13:06:09 +0530

    Warisha_laique

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableHighlight, FlatList } from 'react-native';
 
 export default function App() {
+  const [data, setData] = useState();
+
+  const fetchApiCall = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(post => {
+        setData(post)
+        console.log(data)
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Native API Calls</Text>
+      <TouchableHighlight onPress={fetchApiCall}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Use Fetch API</Text>
+        </View>
+      </TouchableHighlight>
+      <View>
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <View>
+              <Text><Text style={styles.ti}>Title:</Text>{item.title}</Text>
+              <Text><Text style={styles.bo}>Body:</Text>{item.body}</Text>
+            </View>
+
+          )}
+        />
+
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +47,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#AAA',
     alignItems: 'center',
     justifyContent: 'center',
+    color: '#fff'
   },
+  title: {
+    fontSize: 35,
+    color: '#fff'
+  },
+  button: {
+    padding: 10,
+    marginVertical: 15,
+    backgroundColor: '#0645AD'
+  },
+  buttonText: {
+    color: '#fff'
+  },
+  ti: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    color: `#a52a2a`
+  },
+  bo: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    color: `#a52a2a`
+  }
 });
